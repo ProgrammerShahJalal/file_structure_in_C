94121be36d43d6291ae7f37050d13c0b50b68e26
ProgrammerShahJalal <you@example.com>
2022-12-16 00:17:59 +0600

    inputFile

--- ph.c
+++ ph.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 int main()
 {
-    int *inputFile;
+    FILE *inputFile;
+    FILE *outputFile;
     inputFile = fopen("input.txt", "r");
-
+    if(inputFile==NULL)
+    {
+        printf("No file found\n");
+        return 0;
+    }
+    outputFile = fopen("output.txt" ,"w");
     while (1)
     {
         char ch = fgetc(inputFile);
         if (ch == EOF)
             break;
-        printf("%c", ch);
+       fputc(ch, outputFile);
     }
 
     return 0;
